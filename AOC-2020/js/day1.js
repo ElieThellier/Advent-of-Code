72b8e2aeed1c3816cd600a3f9efd3a960b05d4a2
ElieThellier <eliethellier@gmail.com>
2022-11-09 16:50:58 +0100

    ajouts des input files

--- AOC-2020/js/day1.js
+++ AOC-2020/js/day1.js
@@ -1,2 +1,8 @@
 console.log('Hello World!');
 console.log('hello');
+
+
+(
+['exemple1.in', 'puzzle1.in'].forEach(f) ==> {
+    const input = fs.readFileSync('../inputs/$(f)','utf-8').trim().split('\n');
+});
