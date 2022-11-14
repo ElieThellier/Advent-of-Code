1d021b8d1f52ec5103642ee89f17c13efd91b30b
ElieThellier <eliethellier@gmail.com>
2022-11-14 16:01:58 +0100

    ajout de la sol naive pour part2

--- AOC-2020/js/day1/day1.js
+++ AOC-2020/js/day1/day1.js
@@ -10,6 +10,7 @@ const input2 = readFileSync('./inputs/puzzle1.in', 'utf-8')
     .map((x) => parseInt(x));
 
 /*
+// solution naive pour la part 2 (pour la part 1, enlever une boucle)
 const start = Date.now();
 for (let i = 0; i < input2.length; i++) {
     for (let j = 0; j < input2.length; j++) {
@@ -24,7 +25,8 @@ const fin = Date.now() - start;
 console.log(fin);
 */
 
-// utilisation de filter
+/*
+// utilisation de filter part 1
 const start = Date.now();
 for (let i = 0; i < input2.length; i++) {
     let goal = 2020 - input2[i];
@@ -32,6 +34,22 @@ for (let i = 0; i < input2.length; i++) {
     if (filtered.length == 1) {
         console.log(filtered[0] * input2[i]);
         break;
+    }
+}
+const fin = Date.now() - start;
+console.log(fin);
+*/
+
+// utilisation de filter + solution naive pour faire la part 2
+const start = Date.now();
+for (let i = 0; i < input2.length; i++) {
+    for (let j = 0; j < input2.length; j++) {
+        let goal = 2020 - input2[i] - input2[j];
+        let filtered = input2.filter((x) => x == goal);
+        if (filtered.length == 1) {
+            console.log(filtered[0] * input2[i] * input2[j]);
+            break;
+        }
     }
 }
 const fin = Date.now() - start;
