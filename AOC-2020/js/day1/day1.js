a514ce6c9f36cefe0ca6845b05a60aca6993f801
ElieThellier <eliethellier@gmail.com>
2022-11-14 16:23:36 +0100

    amélioration de l'algo avec sort + filter

--- AOC-2020/js/day1/day1.js
+++ AOC-2020/js/day1/day1.js
@@ -1,5 +1,7 @@
 import { readFileSync } from 'fs';
 
+const start = Date.now();
+
 const input1 = readFileSync('./inputs/exemple1.in', 'utf-8')
     .trim()
     .split('\n')
@@ -9,9 +11,11 @@ const input2 = readFileSync('./inputs/puzzle1.in', 'utf-8')
     .split('\n')
     .map((x) => parseInt(x));
 
+input1.sort((a, b) => a - b);
+input2.sort((a, b) => a - b);
+
 /*
 // solution naive pour la part 2 (pour la part 1, enlever une boucle)
-const start = Date.now();
 for (let i = 0; i < input2.length; i++) {
     for (let j = 0; j < input2.length; j++) {
         for (let k = 0; k < input2.length; k++) {
@@ -21,13 +25,10 @@ for (let i = 0; i < input2.length; i++) {
         }
     }
 }
-const fin = Date.now() - start;
-console.log(fin);
 */
 
 /*
 // utilisation de filter part 1
-const start = Date.now();
 for (let i = 0; i < input2.length; i++) {
     let goal = 2020 - input2[i];
     let filtered = input2.filter((x) => x == goal);
@@ -36,21 +37,20 @@ for (let i = 0; i < input2.length; i++) {
         break;
     }
 }
-const fin = Date.now() - start;
-console.log(fin);
 */
 
 // utilisation de filter + solution naive pour faire la part 2
-const start = Date.now();
-for (let i = 0; i < input2.length; i++) {
-    for (let j = 0; j < input2.length; j++) {
-        let goal = 2020 - input2[i] - input2[j];
-        let filtered = input2.filter((x) => x == goal);
-        if (filtered.length == 1) {
-            console.log(filtered[0] * input2[i] * input2[j]);
-            break;
+outerloop: for (let i = 0; i < input2.length; i++) {
+    let filtered1 = input2.filter((x) => x <= 2020 - input2[i]);
+    for (let j = 0; j < filtered1.length; j++) {
+        let goal = 2020 - input2[i] - filtered1[j];
+        let filtered2 = filtered1.filter((x) => x == goal);
+        if (filtered2.length == 1) {
+            console.log(filtered2[0] * input2[i] * filtered1[j]);
+            break outerloop;
         }
     }
 }
+
 const fin = Date.now() - start;
 console.log(fin);
