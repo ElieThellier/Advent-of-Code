fa182b721301a589f779ee41c04684a73e7580de
ElieThellier <eliethellier@gmail.com>
2022-11-23 16:49:11 +0100

    debut du day02 en ts

--- AOC-2020/ts/day02/day02.ts
+++ AOC-2020/ts/day02/day02.ts
@@ -1,5 +1,31 @@
 console.time("Execution Time");
-console.log("2");
-const a = 3;
-console.log(2 * a);
+
+import { readFileSync } from "fs";
+
+const exemple02: string[] = readFileSync(
+    "./day02/inputs02/exemple02.in",
+    "utf-8"
+)
+    .trim()
+    .split("\n");
+
+const puzzle02: string[] = readFileSync("./day02/inputs02/puzzle02.in", "utf-8")
+    .trim()
+    .split("\n");
+
+const min = (line: string) => line.split(" ")[0].split("-")[0];
+const max = (line: string) => line.split(" ")[0].split("-")[1];
+
+const char = (line: string) => line.split(" ")[1][0];
+const word = (line: string) => line.split(" ")[2];
+
+let valid: number = 0;
+exemple02.forEach((line: string) => {
+    let temp = word(line);
+    while (temp.indexOf(char(line)) > -1) {
+        temp = temp.slice(temp.indexOf(char(line)) + 1);
+        valid++;
+    }
+});
+console.log(valid);
 console.timeEnd("Execution Time");
