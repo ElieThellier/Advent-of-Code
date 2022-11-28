bb9779e0f1a4c32f0c8b0dca906b3f6fa588a9ed
ElieThellier <eliethellier@gmail.com>
2022-11-28 21:34:09 +0100

    added day02 part1

--- AOC-2020/ts/day02/day02.ts
+++ AOC-2020/ts/day02/day02.ts
@@ -13,19 +13,27 @@ const puzzle02: string[] = readFileSync("./day02/inputs02/puzzle02.in", "utf-8")
     .trim()
     .split("\n");
 
-const min = (line: string) => line.split(" ")[0].split("-")[0];
-const max = (line: string) => line.split(" ")[0].split("-")[1];
+const min = (line: string) => parseInt(line.split(" ")[0].split("-")[0]);
+const max = (line: string) => parseInt(line.split(" ")[0].split("-")[1]);
 
 const char = (line: string) => line.split(" ")[1][0];
 const word = (line: string) => line.split(" ")[2];
 
-let valid: number = 0;
-exemple02.forEach((line: string) => {
+let valid: number[] = [];
+puzzle02.forEach((line: string) => {
     let temp = word(line);
+    let charCount = 0;
     while (temp.indexOf(char(line)) > -1) {
-        temp = temp.slice(temp.indexOf(char(line)) + 1);
-        valid++;
+        temp = temp.replace(char(line), "");
+        charCount++;
+    }
+    if (charCount >= min(line) && charCount <= max(line)) {
+        valid.push(1);
+    } else {
+        valid.push(0);
     }
 });
 console.log(valid);
+console.log(valid.reduce((acc, val) => acc + val, 0));
+
 console.timeEnd("Execution Time");
