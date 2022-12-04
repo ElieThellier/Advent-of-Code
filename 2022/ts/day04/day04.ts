f076ba98dbd46013ed20c9f59b3fcfb7b2d59452
ElieThellier <eliethellier@gmail.com>
2022-12-04 10:45:11 +0100

    day04 part2

--- 2022/ts/day04/day04.ts
+++ 2022/ts/day04/day04.ts
@@ -21,28 +21,55 @@ const partOne = (input: string) => {
     let maxSecondElves: number[] = secondElves.map((elve) =>
         parseInt(elve.split("-")[1])
     );
+    let countFullyOverlapping: number = 0;
+    pairs.forEach((pair, index) => {
+        if (
+            (minFirstElves[index] <= minSecondElves[index] &&
+                maxFirstElves[index] >= maxSecondElves[index]) ||
+            (minFirstElves[index] >= minSecondElves[index] &&
+                maxFirstElves[index] <= maxSecondElves[index])
+        )
+            countFullyOverlapping++;
+    });
+    return countFullyOverlapping;
+};
+
+const partTwo = (input: string) => {
+    let pairs: string[] = parser(input);
+    let firstElves: string[] = pairs.map((pair) => pair.split(",")[0]);
+    let minFirstElves: number[] = firstElves.map((elve) =>
+        parseInt(elve.split("-")[0])
+    );
+    let maxFirstElves: number[] = firstElves.map((elve) =>
+        parseInt(elve.split("-")[1])
+    );
+    let secondElves: string[] = pairs.map((pair) => pair.split(",")[1]);
+    let minSecondElves: number[] = secondElves.map((elve) =>
+        parseInt(elve.split("-")[0])
+    );
+    let maxSecondElves: number[] = secondElves.map((elve) =>
+        parseInt(elve.split("-")[1])
+    );
     let countOverlapping: number = 0;
     pairs.forEach((pair, index) => {
         if (
-            minFirstElves[index] <= minSecondElves[index] &&
-            maxFirstElves[index] >= maxSecondElves[index]
-        ) {
+            (minFirstElves[index] <= minSecondElves[index] &&
+                maxFirstElves[index] >= maxSecondElves[index]) ||
+            (minFirstElves[index] >= minSecondElves[index] &&
+                maxFirstElves[index] <= maxSecondElves[index]) ||
+            (minFirstElves[index] <= minSecondElves[index] &&
+                maxFirstElves[index] >= minSecondElves[index]) ||
+            (minFirstElves[index] >= minSecondElves[index] &&
+                maxFirstElves[index] <= minSecondElves[index]) ||
+            (minFirstElves[index] <= maxSecondElves[index] &&
+                maxFirstElves[index] >= maxSecondElves[index]) ||
+            (minFirstElves[index] >= maxSecondElves[index] &&
+                maxFirstElves[index] <= maxSecondElves[index])
+        )
             countOverlapping++;
-        } else {
-            if (
-                minFirstElves[index] >= minSecondElves[index] &&
-                maxFirstElves[index] <= maxSecondElves[index]
-            ) {
-                countOverlapping++;
-            }
-        }
     });
     return countOverlapping;
 };
-
-const partTwo = (input: string) => {};
-
-//console.log(partTwo("exemple"));
 
 const logResults = (inputs: string[]) => {
     inputs.forEach((input) =>
@@ -52,13 +79,13 @@ const logResults = (inputs: string[]) => {
             )}_`
         )
     );
-    /* inputs.forEach((input) =>
+    inputs.forEach((input) =>
         console.log(
             `PART 2 : The total fully overlapping pairs for the input "${input}" is equal to _${partTwo(
                 input
             )}_`
         )
-    ); */
+    );
 };
 
 logResults(["exemple", "puzzle"]);
