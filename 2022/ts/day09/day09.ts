3413adb200581b98b7a7fafa9a8f62e7afa8bce6
ElieThellier <eliethellier@gmail.com>
2022-12-10 12:12:24 +0100

    day09 part2 (can be refactored)

--- 2022/ts/day09/day09.ts
+++ 2022/ts/day09/day09.ts
@@ -1,8 +1,12 @@
 import * as fs from "fs";
 
-const timer = (script: (i: string) => any, input: string) => {
+const timer = (
+    script: (i: string, l: number) => any,
+    input: string,
+    ropeLenght: number
+) => {
     const start = performance.now();
-    script(input);
+    script(input, ropeLenght);
     const end = performance.now();
     return `${(end - start).toFixed(2)} ms`;
 };
@@ -15,70 +19,134 @@ const dist = (P: number[], Q: number[]) => {
     );
 };
 
-const partOne = (input: string) => {
+const partsOneAndTwo = (input: string, ropeLenght: number) => {
     const moves = fs
         .readFileSync(`./day09/inputs/${input}.in`, "utf-8")
         .split("\n")
         .map((line) => line.split(" "));
-    let headPositions = [[0, 0]];
-    let tailPositions = [[0, 0]];
-    let nTimesTailVisitedEachPos = [1];
+    let headPositions: number[][][] = [[[0, 0]]];
+    let tailPositions: number[][][] = [];
+    let nTimesTailVisitedEachPos: number[][] = [[1]];
+    let countTailVisitedPos: number[] = [];
+    for (let n = 0; n < ropeLenght; n++) {
+        headPositions.push([[0, 0]]);
+        tailPositions.push([[0, 0]]);
+        nTimesTailVisitedEachPos.push([1]);
+        countTailVisitedPos.push(0);
+    }
+    nTimesTailVisitedEachPos.pop();
     moves.forEach((move) => {
         for (let j = 0; j < parseInt(move[1]); j++) {
             switch (move[0]) {
                 case "R":
-                    headPositions.push([
-                        headPositions[headPositions.length - 1][0] + 1,
-                        headPositions[headPositions.length - 1][1],
+                    headPositions[0].push([
+                        headPositions[0][headPositions[0].length - 1][0] + 1,
+                        headPositions[0][headPositions[0].length - 1][1],
                     ]);
                     break;
                 case "U":
-                    headPositions.push([
-                        headPositions[headPositions.length - 1][0],
-                        headPositions[headPositions.length - 1][1] + 1,
+                    headPositions[0].push([
+                        headPositions[0][headPositions[0].length - 1][0],
+                        headPositions[0][headPositions[0].length - 1][1] + 1,
                     ]);
                     break;
                 case "L":
-                    headPositions.push([
-                        headPositions[headPositions.length - 1][0] - 1,
-                        headPositions[headPositions.length - 1][1],
+                    headPositions[0].push([
+                        headPositions[0][headPositions[0].length - 1][0] - 1,
+                        headPositions[0][headPositions[0].length - 1][1],
                     ]);
                     break;
                 case "D":
-                    headPositions.push([
-                        headPositions[headPositions.length - 1][0],
-                        headPositions[headPositions.length - 1][1] - 1,
+                    headPositions[0].push([
+                        headPositions[0][headPositions[0].length - 1][0],
+                        headPositions[0][headPositions[0].length - 1][1] - 1,
                     ]);
                     break;
             }
-            if (
-                dist(
-                    headPositions[headPositions.length - 1],
-                    tailPositions[tailPositions.length - 1]
-                ) >= 2
-            ) {
-                tailPositions.push(headPositions[headPositions.length - 2]);
-            } else {
-                tailPositions.push(tailPositions[tailPositions.length - 1]);
-            }
-            let countTimesTailVisitedPos = 1;
-            for (let pos of tailPositions.slice(0, tailPositions.length - 1)) {
-                if (
-                    tailPositions[tailPositions.length - 1][0] === pos[0] &&
-                    tailPositions[tailPositions.length - 1][1] === pos[1]
-                ) {
-                    countTimesTailVisitedPos++;
+            for (let n = 0; n < ropeLenght; n++) {
+                let temp1 = tailPositions[n][tailPositions[n].length - 1];
+                let temp2 = headPositions[n][tailPositions[n].length + 1 - 1];
+                if (dist(temp1, temp2) >= 2) {
+                    if (temp1[0] === temp2[0]) {
+                        if (temp2[1] > temp1[1]) {
+                            tailPositions[n].push([temp1[0], temp1[1] + 1]);
+                        } else if (temp2[1] < temp1[1]) {
+                            tailPositions[n].push([temp1[0], temp1[1] - 1]);
+                        }
+                    } else if (temp1[1] === temp2[1]) {
+                        if (temp2[0] > temp1[0]) {
+                            tailPositions[n].push([temp1[0] + 1, temp1[1]]);
+                        } else if (temp2[0] < temp1[0]) {
+                            tailPositions[n].push([temp1[0] - 1, temp1[1]]);
+                        }
+                    } else if (temp2[0] > temp1[0]) {
+                        if (temp2[1] > temp1[1]) {
+                            tailPositions[n].push([temp1[0] + 1, temp1[1] + 1]);
+                        } else if (temp2[1] < temp1[1]) {
+                            tailPositions[n].push([temp1[0] + 1, temp1[1] - 1]);
+                        }
+                    } else if (temp2[0] < temp1[0]) {
+                        if (temp2[1] > temp1[1]) {
+                            tailPositions[n].push([temp1[0] - 1, temp1[1] + 1]);
+                        } else if (temp2[1] < temp1[1]) {
+                            tailPositions[n].push([temp1[0] - 1, temp1[1] - 1]);
+                        }
+                    }
+                } else {
+                    tailPositions[n].push(temp1);
                 }
+                headPositions[n + 1] = tailPositions[n];
+
+                let countTimesTailVisitedPos = 1;
+                for (let pos of tailPositions[n].slice(
+                    0,
+                    tailPositions[n].length - 1
+                )) {
+                    if (
+                        tailPositions[n][tailPositions[n].length - 1][0] ===
+                            pos[0] &&
+                        tailPositions[n][tailPositions[n].length - 1][1] ===
+                            pos[1]
+                    ) {
+                        countTimesTailVisitedPos++;
+                    }
+                }
+                nTimesTailVisitedEachPos[n].push(countTimesTailVisitedPos);
+
+                countTailVisitedPos[n] = nTimesTailVisitedEachPos[n].reduce(
+                    (prev, curr) => (curr === 1 ? prev + curr : prev + 0),
+                    0
+                );
             }
-            nTimesTailVisitedEachPos.push(countTimesTailVisitedPos);
         }
     });
-    let countTailVisitedPos = nTimesTailVisitedEachPos.reduce(
-        (prev, curr) => (curr === 1 ? prev + curr : prev + 0),
-        0
-    );
-    console.log(countTailVisitedPos);
+    return countTailVisitedPos[ropeLenght - 1];
 };
 
-partOne("exemple");
-partOne("puzzle");
+["exemple1", "exemple2", "puzzle"].forEach((input) => {
+    console.log(
+        `PART 1 : The number of positions visited by the tail of the rope is _${partsOneAndTwo(
+            input,
+            1
+        )}_ (rope's lenght is 1 and input is "${input}" (executed in ${timer(
+            partsOneAndTwo,
+            input,
+            1
+        )})`
+    );
+});
+
+console.log("\n");
+
+["exemple1", "exemple2", "puzzle"].forEach((input) => {
+    console.log(
+        `PART 2 : The number of positions visited by the tail of the rope is _${partsOneAndTwo(
+            input,
+            9
+        )}_ (rope's lenght is 9 and input is "${input}" (executed in ${timer(
+            partsOneAndTwo,
+            input,
+            9
+        )})`
+    );
+});
