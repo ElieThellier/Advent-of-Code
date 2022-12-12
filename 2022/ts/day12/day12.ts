2f47f74ee34a991f460353dfbcce995a869e9a58
ElieThellier <eliethellier@gmail.com>
2022-12-12 22:10:15 +0100

    day12 part1 & part2

--- 2022/ts/day12/day12.ts
+++ 2022/ts/day12/day12.ts
@@ -1,128 +1,254 @@
 import * as fs from "fs";
 
-const timer = (
-    script: (i: string, p1: boolean) => number,
-    input: string,
-    part1: boolean
-) => {
+const timer = (script: (i: string) => number, input: string) => {
     const start = performance.now();
-    script(input, part1);
+    script(input);
     const end = performance.now();
     return `${(end - start).toFixed(2)} ms`;
 };
 
 type Cell = {
-    height: number; // CharAt('a')
-    visited: boolean;
-    isCurrentPos: boolean; // CharAt('S')
-    isBestSignal: boolean; // CharAt('E')
+    height: string;
     posX: number;
     posY: number;
 };
 
 const initiateCell = (myCell: string, x: number, y: number) => {
     const resCell: Cell = {
-        height:
-            myCell === "S"
-                ? "a".charCodeAt(0) - "a".charCodeAt(0) + 1
-                : myCell === "E"
-                ? "z".charCodeAt(0) - "a".charCodeAt(0) + 1
-                : myCell.charCodeAt(0) - "a".charCodeAt(0) + 1,
-        visited: myCell === "S" ? true : false,
-        isCurrentPos: myCell === "S" ? true : false,
-        isBestSignal: myCell === "E" ? true : false,
+        height: myCell,
         posX: x,
         posY: y,
     };
     return resCell;
 };
 
-const partOne = (input: string) => {
-    let currentCell: Cell = initiateCell("a", 0, 0);
-    const grid: Cell[][] = fs
-        .readFileSync(`./day12/inputs/${input}.in`, "utf-8")
-        .split("\n")
-        .map((line, y) =>
-            line.split("").map((cell, x) => {
-                cell === "S" && (currentCell = initiateCell(cell, x, y));
-                return initiateCell(cell, x, y);
-            })
-        );
-
-    const width = grid[0].length;
-    const height = grid.length;
-
-    let instruc: number;
-
-    while (!currentCell.isBestSignal) {
-        instruc = Math.round(3 * Math.random());
-        // 0 for up, 1 for right, 2 for bot, 3 for left
-        switch (instruc) {
-            case 0:
-                if (currentCell.isBestSignal) {
-                    return [currentCell.posX, currentCell.posY];
-                }
-                if (currentCell.posY == 0) break;
-                else if (
-                    grid[currentCell.posX][currentCell.posY - 1].height ===
-                        currentCell.height + 1 ||
-                    grid[currentCell.posX][currentCell.posY - 1].height ===
-                        currentCell.height
-                ) {
-                    currentCell = grid[currentCell.posX][currentCell.posY - 1];
-                    currentCell.visited = true;
-                    console.log(currentCell.posX, currentCell.posY);
-                } else break;
-
-            case 1:
-                if (currentCell.isBestSignal) {
-                    return [currentCell.posX, currentCell.posY];
-                }
-                if (currentCell.posX == width - 1) break;
-                else if (
-                    grid[currentCell.posX + 1][currentCell.posY].height ===
-                        currentCell.height + 1 ||
-                    grid[currentCell.posX + 1][currentCell.posY].height ===
-                        currentCell.height
-                ) {
-                    currentCell = grid[currentCell.posX + 1][currentCell.posY];
-                    currentCell.visited = true;
-                    console.log(currentCell.posX, currentCell.posY);
-                } else break;
-            case 2:
-                if (currentCell.isBestSignal) {
-                    return [currentCell.posX, currentCell.posY];
-                }
-                if (currentCell.posY == height - 1) break;
-                else if (
-                    grid[currentCell.posX][currentCell.posY + 1].height ===
-                        currentCell.height + 1 ||
-                    grid[currentCell.posX][currentCell.posY + 1].height ===
-                        currentCell.height
-                ) {
-                    currentCell = grid[currentCell.posX][currentCell.posY + 1];
-                    currentCell.visited = true;
-                    console.log(currentCell.posX, currentCell.posY);
-                } else break;
-            case 3:
-                if (currentCell.isBestSignal) {
-                    return [currentCell.posX, currentCell.posY];
-                }
-                if (currentCell.posX == 0) break;
-                else if (
-                    grid[currentCell.posX - 1][currentCell.posY].height ===
-                        currentCell.height + 1 ||
-                    grid[currentCell.posX - 1][currentCell.posY].height ===
-                        currentCell.height
-                ) {
-                    currentCell = grid[currentCell.posX - 1][currentCell.posY];
-                    currentCell.visited = true;
-                    console.log(currentCell.posX, currentCell.posY);
-                } else break;
-            default:
-                break;
+const getNeighbors = (cell: Cell, grid: Cell[][]) => {
+    let neighbors = new Array();
+    let width = grid[0].length - 1;
+    let height = grid.length - 1;
+    if (cell.posX > 0) {
+        if (
+            grid[cell.posY][cell.posX - 1].height.charCodeAt(0) <=
+            cell.height.charCodeAt(0) + 1
+        ) {
+            neighbors.push(grid[cell.posY][cell.posX - 1]);
         }
     }
+    if (cell.posY > 0) {
+        if (
+            grid[cell.posY - 1][cell.posX].height.charCodeAt(0) <=
+            cell.height.charCodeAt(0) + 1
+        ) {
+            neighbors.push(grid[cell.posY - 1][cell.posX]);
+        }
+    }
+    if (cell.posX < width) {
+        if (
+            grid[cell.posY][cell.posX + 1].height.charCodeAt(0) <=
+            cell.height.charCodeAt(0) + 1
+        ) {
+            neighbors.push(grid[cell.posY][cell.posX + 1]);
+        }
+    }
+    if (cell.posY < height) {
+        if (
+            grid[cell.posY + 1][cell.posX].height.charCodeAt(0) <=
+            cell.height.charCodeAt(0) + 1
+        ) {
+            neighbors.push(grid[cell.posY + 1][cell.posX]);
+        }
+    }
+    return neighbors;
 };
 
-partOne("exemple");
+const getPath = (
+    grid: string[][],
+    gridCell: Cell[][],
+    startingCell: Cell,
+    endingCell: Cell
+) => {
+    // A* algorithm implementation
+    let frontier = [];
+    frontier.push(startingCell);
+    let came_from = new Map();
+    came_from.set(`${[startingCell.posY, startingCell.posX]}`, null);
+    let currentCell = startingCell;
+    while (!(frontier.length === 0)) {
+        currentCell = frontier.shift();
+        if (
+            grid[currentCell.posY][currentCell.posX] ===
+            grid[endingCell.posY][endingCell.posX]
+        ) {
+            break;
+        }
+        for (let next of getNeighbors(currentCell, gridCell)) {
+            if (!came_from.has(`${[next.posY, next.posX]}`)) {
+                frontier.push(next);
+                came_from.set(
+                    `${[next.posY, next.posX]}`,
+                    `${[currentCell.posY, currentCell.posX]}`
+                );
+            }
+        }
+    }
+    return came_from;
+};
+
+const getPathLenght = (
+    possibleMove: Map<any, any>,
+    startingCell: Cell,
+    endingCell: Cell
+) => {
+    let cell = `${[endingCell.posY, endingCell.posX]}`;
+    let path = [];
+    while (cell !== `${[startingCell.posY, startingCell.posX]}`) {
+        path.push(cell);
+        cell = possibleMove.get(cell);
+    }
+    path.reverse();
+
+    return path;
+};
+
+const partOne = (input: string) => {
+    let startingCell: Cell = initiateCell("a", 0, 0);
+    let endingCell: Cell = initiateCell("a", 0, 0);
+    const grid: string[][] = fs
+        .readFileSync(`./day12/inputs/${input}.in`, "utf-8")
+        .split("\n")
+        .map((line) => line.split(""));
+    const gridCell: Cell[][] = grid.map((line, y) =>
+        line.map((cell, x) => {
+            cell === "S" && (startingCell = initiateCell("a", x, y));
+            cell === "E" && (endingCell = initiateCell("z", x, y));
+            if (cell === "S") {
+                return initiateCell("a", x, y);
+            }
+            if (cell === "E") {
+                return initiateCell("z", x, y);
+            }
+            return initiateCell(cell, x, y);
+        })
+    );
+    let possibleMove = getPath(grid, gridCell, startingCell, endingCell);
+    return getPathLenght(possibleMove, startingCell, endingCell).length;
+};
+
+const getNeighbors2 = (cell: Cell, grid: Cell[][]) => {
+    let neighbors = new Array();
+    let width = grid[0].length - 1;
+    let height = grid.length - 1;
+    if (cell.posX > 0) {
+        if (
+            grid[cell.posY][cell.posX - 1].height.charCodeAt(0) + 1 >=
+            cell.height.charCodeAt(0)
+        ) {
+            neighbors.push(grid[cell.posY][cell.posX - 1]);
+        }
+    }
+    if (cell.posY > 0) {
+        if (
+            grid[cell.posY - 1][cell.posX].height.charCodeAt(0) + 1 >=
+            cell.height.charCodeAt(0)
+        ) {
+            neighbors.push(grid[cell.posY - 1][cell.posX]);
+        }
+    }
+    if (cell.posX < width) {
+        if (
+            grid[cell.posY][cell.posX + 1].height.charCodeAt(0) + 1 >=
+            cell.height.charCodeAt(0)
+        ) {
+            neighbors.push(grid[cell.posY][cell.posX + 1]);
+        }
+    }
+    if (cell.posY < height) {
+        if (
+            grid[cell.posY + 1][cell.posX].height.charCodeAt(0) + 1 >=
+            cell.height.charCodeAt(0)
+        ) {
+            neighbors.push(grid[cell.posY + 1][cell.posX]);
+        }
+    }
+    return neighbors;
+};
+
+const getPath2 = (
+    grid: string[][],
+    gridCell: Cell[][],
+    startingCell: Cell,
+    endingCell: string
+) => {
+    // A* algorithm implementation
+    let frontier = [];
+    frontier.push(startingCell);
+    let came_from = new Map();
+    came_from.set(`${[startingCell.posY, startingCell.posX]}`, null);
+    let currentCell = startingCell;
+    let findFirstEnd = currentCell;
+    while (!(frontier.length === 0)) {
+        currentCell = frontier.shift();
+        if (grid[currentCell.posY][currentCell.posX] === endingCell) {
+            findFirstEnd = currentCell;
+            break;
+        }
+        for (let next of getNeighbors2(currentCell, gridCell)) {
+            if (!came_from.has(`${[next.posY, next.posX]}`)) {
+                frontier.push(next);
+                came_from.set(
+                    `${[next.posY, next.posX]}`,
+                    `${[currentCell.posY, currentCell.posX]}`
+                );
+            }
+        }
+    }
+    return [findFirstEnd, came_from];
+};
+
+const partTwo = (input: string) => {
+    let startingCell: Cell = initiateCell("a", 0, 0);
+    let endingCell: Cell = initiateCell("a", 0, 0);
+    const grid: string[][] = fs
+        .readFileSync(`./day12/inputs/${input}.in`, "utf-8")
+        .split("\n")
+        .map((line) => line.split(""));
+    const gridCell: Cell[][] = grid.map((line, y) =>
+        line.map((cell, x) => {
+            cell === "S" && (startingCell = initiateCell("a", x, y));
+            cell === "E" && (endingCell = initiateCell("z", x, y));
+            if (cell === "S") {
+                return initiateCell("a", x, y);
+            }
+            if (cell === "E") {
+                return initiateCell("z", x, y);
+            }
+            return initiateCell(cell, x, y);
+        })
+    );
+    let getRes: any[] = [];
+    for (let elt of getPath2(grid, gridCell, endingCell, "a")) {
+        getRes.push(elt);
+    }
+    let possibleMove = getRes[1];
+    let bestStart = getRes[0];
+
+    return getPathLenght(possibleMove, endingCell, bestStart).length;
+};
+
+["exemple", "puzzle"].forEach((input) => {
+    console.log(
+        `PART 1 "${input}" : The fewest steps required to move from your current position to the location with the best signal is _${partOne(
+            input
+        )}_ (executed in ${timer(partOne, input)})`
+    );
+});
+console.log("");
+
+["exemple", "puzzle"].forEach((input) => {
+    console.log(
+        `PART 2 "${input}" : The fewest steps required to move starting from any square with elevation a to the location with the best signal is _${partTwo(
+            input
+        )}_ (executed in ${timer(partTwo, input)})`
+    );
+});
+console.log("");
