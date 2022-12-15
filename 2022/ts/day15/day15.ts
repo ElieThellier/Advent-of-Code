9820ab660f00035febef3b967100a002ecef8ca4
ElieThellier <eliethellier@gmail.com>
2022-12-15 23:57:51 +0100

    day15 part1

--- 2022/ts/day15/day15.ts
+++ 2022/ts/day15/day15.ts
@@ -8,12 +8,12 @@ const timer = (script: (i: string) => number, input: string) => {
 };
 
 const parseAndGetExtremums = (input: string) => {
-    // input line by line
+    // input line by line and split by space
     const lines = fs
         .readFileSync(`./day15/inputs/${input}.in`, "utf-8")
         .split("\n")
         .map((line) => line.split(" "));
-    // rocks without line between rocks
+    // get coordonates of sensors and beacons in correspondance
     let pointsCorrespondance = lines.map((line) => {
         return [
             [
@@ -48,12 +48,13 @@ const parseAndGetExtremums = (input: string) => {
         )
     );
 
-    let offset = 100_000;
-
+    // offset to include # in theLine that are not in the starting grid
+    // I started at 2_000_000 and decreased the value (to lower execution time) while I was getting the right answer
+    // an offset of 650_000 is enough to get the right answer FOR MY PUZZLE INPUT (MAY NOT WORK FOR YOURS)
+    let offset = 650_000;
     if (input === "example") {
         offset = 1;
     }
-
     minX -= offset;
     maxX += offset;
 
@@ -64,23 +65,18 @@ const parseAndGetExtremums = (input: string) => {
                 return [point[0] + Math.abs(minX), point[1] + Math.abs(minY)];
             })
     );
-
     return [pointsCorrespondance, minX, maxX, minY, maxY];
 };
 
-const createGrid = (minX: any, maxX: any, minY: any, maxY: any) => {
+const createTheLine = (minX: any, maxX: any) => {
     let width = maxX + Math.abs(minX) + 1;
-    let height = maxY + Math.abs(minY) + 1;
 
-    // init grid with dots
-    let grid = new Array();
-    for (let y = 0; y < height; y++) {
-        grid.push([]);
-        for (let x = 0; x < width; x++) {
-            grid[y].push(".");
-        }
+    // init theLine with dots
+    let theLine = new Array();
+    for (let x = 0; x < width; x++) {
+        theLine.push(".");
     }
-    return [grid, width, height];
+    return [theLine, width];
 };
 
 const dist = (p1: number[], p2: number[]) => {
@@ -90,14 +86,13 @@ const dist = (p1: number[], p2: number[]) => {
 const partOne = (input: string) => {
     let [pointsCorrespondance, minX, maxX, minY, maxY]: any[] =
         parseAndGetExtremums(input);
-    let [grid, width, height]: any[] = createGrid(minX, maxX, minY, maxY);
+    let [theLine, width]: any[] = createTheLine(minX, maxX);
     let distSB: number[] = [];
     pointsCorrespondance.forEach((correspondance: number[][]) => {
         distSB.push(dist(correspondance[0], correspondance[1]));
     });
 
     let row = 2_000_000 + Math.abs(minY);
-
     if (input === "example") {
         row = 10 + Math.abs(minY);
     }
@@ -107,6 +102,7 @@ const partOne = (input: string) => {
         distToRow.push(Math.abs(correspondance[0][1] - row));
     });
 
+    // get interesting sensors (sensors that can see the row)
     let interestingSensors: number[][][] = [];
     let distSBofInterestingSensors: number[] = [];
 
@@ -117,28 +113,42 @@ const partOne = (input: string) => {
         }
     });
 
-    // draw range of interesting sensors
+    // draw range of interesting sensors on theLine
     interestingSensors.forEach((sensor: number[][], i) => {
-        for (let y = 0; y < height; y++) {
-            for (let x = 0; x < width; x++) {
-                if (dist([x, y], sensor[0]) <= distSBofInterestingSensors[i]) {
-                    grid[y][x] = "#";
-                }
+        for (let x = 0; x < width; x++) {
+            if (dist([x, row], sensor[0]) <= distSBofInterestingSensors[i]) {
+                theLine[x] = "#";
             }
         }
     });
 
+    // draw sensors and beacons
     for (let correspondance of pointsCorrespondance) {
-        grid[correspondance[0][1]][correspondance[0][0]] = "S";
-        grid[correspondance[1][1]][correspondance[1][0]] = "B";
+        if (correspondance[0][1] === row) {
+            theLine[correspondance[0][0]] = "S";
+        }
+        if (correspondance[1][1] === row) {
+            theLine[correspondance[1][0]] = "B";
+        }
     }
 
-    // pretty print grid (uncomment to see / too big with puzzle input)
-    //console.log(grid.map((line: string[]) => line.join("")).join("\n"));
+    // pretty print theLine (uncomment to see / too big with puzzle input)
+    //console.log(theLine.join(""));
 
-    return grid[row].filter((c: string) => c === "#").length;
+    return theLine.filter((c: string) => c === "#").length;
 };
 
 console.log('\n--- Day 15: "Beacon Exclusion Zone" ---\n');
 
-console.log(partOne("puzzle"));
+["example", "puzzle"].forEach((input) => {
+    console.log(
+        `PART 1 "${input}" : _${partOne(
+            input
+        )}_ positions cannot contain a beacon in the selected row (executed in ${timer(
+            partOne,
+            input
+        )})`
+    );
+});
+
+// TODO: part 2
