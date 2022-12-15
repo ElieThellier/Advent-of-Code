276259a18288fba0b08cef0eecfb620b9e7e0537
ElieThellier <eliethellier@gmail.com>
2022-12-15 23:15:33 +0100

    FATAL ERROR Aborded :s

--- 2022/ts/day15/day15.ts
+++ 2022/ts/day15/day15.ts
@@ -14,7 +14,7 @@ const parseAndGetExtremums = (input: string) => {
         .split("\n")
         .map((line) => line.split(" "));
     // rocks without line between rocks
-    let pointsCorespondance = lines.map((line) => {
+    let pointsCorrespondance = lines.map((line) => {
         return [
             [
                 parseInt(line[2].split("=")[1].slice(0, -1)),
@@ -28,43 +28,49 @@ const parseAndGetExtremums = (input: string) => {
     });
     // extremum pos for each coordonate
     let minX = Math.min(
-        ...pointsCorespondance.map((corespondance) =>
-            Math.min(corespondance[0][0], corespondance[1][0])
+        ...pointsCorrespondance.map((correspondance) =>
+            Math.min(correspondance[0][0], correspondance[1][0])
         )
     );
     let maxX = Math.max(
-        ...pointsCorespondance.map((corespondance) =>
-            Math.max(corespondance[0][0], corespondance[1][0])
+        ...pointsCorrespondance.map((correspondance) =>
+            Math.max(correspondance[0][0], correspondance[1][0])
         )
     );
     let minY = Math.min(
-        ...pointsCorespondance.map((corespondance) =>
-            Math.min(corespondance[0][1], corespondance[1][1])
+        ...pointsCorrespondance.map((correspondance) =>
+            Math.min(correspondance[0][1], correspondance[1][1])
         )
     );
     let maxY = Math.max(
-        ...pointsCorespondance.map((corespondance) =>
-            Math.max(corespondance[0][1], corespondance[1][1])
+        ...pointsCorrespondance.map((correspondance) =>
+            Math.max(correspondance[0][1], correspondance[1][1])
         )
     );
-    return [pointsCorespondance, minX, maxX, minY, maxY];
+
+    let offset = 100_000;
+
+    if (input === "example") {
+        offset = 1;
+    }
+
+    minX -= offset;
+    maxX += offset;
+
+    // translate all points to positive
+    pointsCorrespondance = pointsCorrespondance.map(
+        (correspondance: number[][]) =>
+            correspondance.map((point: number[]) => {
+                return [point[0] + Math.abs(minX), point[1] + Math.abs(minY)];
+            })
+    );
+
+    return [pointsCorrespondance, minX, maxX, minY, maxY];
 };
 
-const createGrid = (
-    pointsCorespondance: any,
-    minX: any,
-    maxX: any,
-    minY: any,
-    maxY: any
-) => {
+const createGrid = (minX: any, maxX: any, minY: any, maxY: any) => {
     let width = maxX + Math.abs(minX) + 1;
     let height = maxY + Math.abs(minY) + 1;
-
-    pointsCorespondance = pointsCorespondance.map((corespondance: number[][]) =>
-        corespondance.map((point: number[], i) => {
-            return [point[0] + Math.abs(minX), point[1] + Math.abs(minY)];
-        })
-    );
 
     // init grid with dots
     let grid = new Array();
@@ -74,21 +80,65 @@ const createGrid = (
             grid[y].push(".");
         }
     }
-    // add sensors and beacons
-    // ATTENTION grid[y][x]
-    for (let corespondance of pointsCorespondance) {
-        grid[corespondance[0][1]][corespondance[0][0]] = "S";
-        grid[corespondance[1][1]][corespondance[1][0]] = "B";
-    }
-    return grid;
+    return [grid, width, height];
+};
+
+const dist = (p1: number[], p2: number[]) => {
+    return Math.abs(p2[1] - p1[1]) + Math.abs(p2[0] - p1[0]);
 };
 
 const partOne = (input: string) => {
-    let [pointsCorespondance, minX, maxX, minY, maxY] =
+    let [pointsCorrespondance, minX, maxX, minY, maxY]: any[] =
         parseAndGetExtremums(input);
-    let grid = createGrid(pointsCorespondance, minX, maxX, minY, maxY);
-    console.log(grid.map((line) => line.join("")).join("\n"));
+    let [grid, width, height]: any[] = createGrid(minX, maxX, minY, maxY);
+    let distSB: number[] = [];
+    pointsCorrespondance.forEach((correspondance: number[][]) => {
+        distSB.push(dist(correspondance[0], correspondance[1]));
+    });
+
+    let row = 2_000_000 + Math.abs(minY);
+
+    if (input === "example") {
+        row = 10 + Math.abs(minY);
+    }
+
+    let distToRow: number[] = [];
+    pointsCorrespondance.forEach((correspondance: number[][]) => {
+        distToRow.push(Math.abs(correspondance[0][1] - row));
+    });
+
+    let interestingSensors: number[][][] = [];
+    let distSBofInterestingSensors: number[] = [];
+
+    distSB.forEach((d, i) => {
+        if (d >= distToRow[i]) {
+            interestingSensors.push(pointsCorrespondance[i]);
+            distSBofInterestingSensors.push(d);
+        }
+    });
+
+    // draw range of interesting sensors
+    interestingSensors.forEach((sensor: number[][], i) => {
+        for (let y = 0; y < height; y++) {
+            for (let x = 0; x < width; x++) {
+                if (dist([x, y], sensor[0]) <= distSBofInterestingSensors[i]) {
+                    grid[y][x] = "#";
+                }
+            }
+        }
+    });
+
+    for (let correspondance of pointsCorrespondance) {
+        grid[correspondance[0][1]][correspondance[0][0]] = "S";
+        grid[correspondance[1][1]][correspondance[1][0]] = "B";
+    }
+
+    // pretty print grid (uncomment to see / too big with puzzle input)
+    //console.log(grid.map((line: string[]) => line.join("")).join("\n"));
+
+    return grid[row].filter((c: string) => c === "#").length;
 };
-partOne("example");
 
 console.log('\n--- Day 15: "Beacon Exclusion Zone" ---\n');
+
+console.log(partOne("puzzle"));
